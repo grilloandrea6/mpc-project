f0d99011fb5045d089b8cb62081e6ad046b6e968
Andrea Grillo <andrea.grillo@epfl.ch>
2023-12-16 10:39:49 +0100

    3.2 started x

--- Deliverable_3_2/Deliverable_3_2.m
+++ Deliverable_3_2/Deliverable_3_2.m
@@ -1,7 +1,93 @@
 addpath(fullfile('..', 'src'));
 
-%close all
-%clear all
-%clc
+close all
+clear all
+clc
 
 %% TODO: This file should produce all the plots for the deliverable
+
+Ts = 1/20;
+rocket = Rocket(Ts);
+
+[xs, us] = rocket.trim(); % Compute steady−state for which 0 = f(xs,us)
+sys = rocket.linearize(xs, us); % Linearize the nonlinear model about trim point
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us); % Decompose the system in 4 independent systems
+
+
+% Design MPC controller
+H = 1; % Horizon length in seconds
+Tf = 10;
+
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+
+% start from 3 meters
+x_x_0 = [0 0 0 0]';
+x_y_0 = [0 0 0 3]';
+x_z_0 = [0 3]';
+x_roll_0 = [0 deg2rad(40)]';
+
+ref_x = -4;
+
+% 
+% %% controller X
+% [u, T_opt, X_opt, U_opt] = mpc_x.get_u(x_x_0,ref_x);
+% U_opt(:,end+1) = NaN;
+% % Account for linearization point
+% X_opt = X_opt + xs([2,5,7,10]);
+% U_opt = U_opt + us(2);
+% % open loop plot
+% rocket.plotvis_sub(T_opt, X_opt, U_opt, sys_x, xs, us, ref_x); % Plot as usual
+% 
+% % closed loop plot
+% [T, X_sub, U_sub] = rocket.simulate_f(sys_x, x_x_0, Tf, @mpc_x.get_u, ref_x);
+% rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us, ref_x);
+
+% 
+% %% controller Y
+% [u, T_opt, X_opt, U_opt] = mpc_y.get_u(x_y_0);
+% U_opt(:,end+1) = NaN;
+% % Account for linearization point
+% X_opt = X_opt + xs([1,4,8,11]);
+% U_opt = U_opt + us(1);
+% % open loop plot
+% rocket.plotvis_sub(T_opt, X_opt, U_opt, sys_y, xs, us); % Plot as usual
+% 
+% % closed loop plot
+% [T, X_sub, U_sub] = rocket.simulate_f(sys_y, x_y_0, Tf, @mpc_y.get_u, 0);
+% rocket.plotvis_sub(T, X_sub, U_sub, sys_y, xs, us);
+
+% 
+% %% controller Z
+% [u, T_opt, X_opt, U_opt] = mpc_z.get_u(x_z_0);
+% U_opt(:,end+1) = NaN;
+% % Account for linearization point
+% X_opt = X_opt + xs([9,12]);
+% U_opt = U_opt + us(3);
+% % open loop plot
+% rocket.plotvis_sub(T_opt, X_opt, U_opt, sys_z, xs, us); % Plot as usual
+% 
+% % closed loop plot
+% [T, X_sub, U_sub] = rocket.simulate_f(sys_z, x_z_0, Tf, @mpc_z.get_u, 0);
+% rocket.plotvis_sub(T, X_sub, U_sub, sys_z, xs, us);
+
+
+% 
+% %% controller roll
+% [u, T_opt, X_opt, U_opt] = mpc_roll.get_u(x_roll_0);
+% U_opt(:,end+1) = NaN;
+% % Account for linearization point
+% X_opt = X_opt + xs([3,6]);
+% U_opt = U_opt + us(4);
+% % open loop plot
+% rocket.plotvis_sub(T_opt, X_opt, U_opt, sys_roll, xs, us); % Plot as usual
+% 
+% % closed loop plot
+% [T, X_sub, U_sub] = rocket.simulate_f(sys_roll, x_z_0, Tf, @mpc_roll.get_u, 0);
+% rocket.plotvis_sub(T, X_sub, U_sub, sys_roll, xs, us);
+% 
---
+++ Deliverable_3_2/MpcControl_x.m
@@ -0,0 +1,114 @@
+classdef MpcControl_x < MpcControlBase
+    
+    methods
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc, Ts, H)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   X(:,1)       - initial state (estimate)
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   U(:,1)       - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            N_segs = ceil(H/Ts); % Horizon steps
+            N = N_segs + 1;      % Last index in 1-based Matlab indexing
+
+            [nx, nu] = size(mpc.B);
+            
+            % Targets (Ignore this before Todo 3.2)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+            
+            % Predicted state and input trajectories
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
+            %       the DISCRETE-TIME MODEL of your system
+            
+            
+            % Horizon and cost matrices
+            Q = 10 * eye(4);
+            R = 1;
+            
+            % Constraints
+            
+            
+            %% TODO not clear if I have to subtract the steady state xs,us in the constraints
+           
+            % u in U = { u | Mu <= m }
+            M = [1;-1]; m = [.26; .26];
+            % x in X = { x | Fx <= f }
+            F = [0 1 0 0; 0 -1 0 0]; f = [.1745; .1745];
+
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+
+            con = (X(:,2) == mpc.A*X(:,1) + mpc.B*U(:,1)) + (M*U(:,1) <= m);
+            obj = (U(:,1) - u_ref)' * R * (U(:,1) - u_ref);
+            for i = 2:N-1
+                con = con + (X(:,i+1) == mpc.A*X(:,i) + mpc.B*U(:,i));
+                con = con + (F*X(:,i) <= f) + (M*U(:,i) <= m);
+                obj = obj + (X(:,i) - x_ref)' * Q * (X(:,i) - x_ref) + (U(:,i) - u_ref)' * R * (U(:,i) - u_ref);
+            end
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {X(:,1), x_ref, u_ref}, {U(:,1), X, U});
+        end
+        
+        % Design a YALMIP optimizer object that takes a position reference
+        % and returns a feasible steady-state state and input (xs, us)
+        function target_opti = setup_steady_state_target(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            % OUTPUTS
+            %   xs, us - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            nx = size(mpc.A, 1);
+
+            % Steady-state targets
+            xs = sdpvar(nx, 1);
+            us = sdpvar;
+            
+            % Reference position (Ignore this before Todo 3.2)
+            ref = sdpvar;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+
+            Rs = 1;
+
+            obj = us' * Rs * us;
+            
+            mat = [eye(size(mpc.A,1))-mpc.A -mpc.B; mpc.C zeros(size(mpc.C,1),size(mpc.B,2))];
+
+            con = ((mat * [xs; us]) == [zeros(size(mpc.A,1),1) ; ref]);
+            
+            % u in U = { u | Mu <= m }
+            M = [1;-1]; m = [.26; .26];
+            % x in X = { x | Fx <= f }
+            F = [0 1 0 0; 0 -1 0 0]; f = [.1745; .1745];
+            
+            con = con + (F*xs <= f) + (M*us <= m);
+
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute the steady-state target
+            target_opti = optimizer(con, obj, sdpsettings('solver', 'gurobi'), ref, {xs, us});
+        end
+    end
+end
