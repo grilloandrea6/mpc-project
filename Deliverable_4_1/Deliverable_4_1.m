19d6738dedb9e16ba72c4869a91ee059d6ec580b
Andrea Grillo <andrea.grillo@epfl.ch>
2023-12-22 12:49:51 +0100

    4.1 code cleaning

--- Deliverable_4_1/Deliverable_4_1.m
+++ Deliverable_4_1/Deliverable_4_1.m
@@ -4,23 +4,22 @@ close all
 clear all
 clc
 
-Ts = 1/20;
+% Constant definition
+Ts = 1/20; % Sampling time
+H = 5; % Horizon length in seconds
+Tf = 35; % Simulation time
+
 rocket = Rocket(Ts);
 
 [xs, us] = rocket.trim(); % Compute steady−state for which 0 = f(xs,us)
 sys = rocket.linearize(xs, us); % Linearize the nonlinear model about trim point
 [sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us); % Decompose the system in 4 independent systems
 
-
-% Design MPC controller
-H = 10; % Horizon length in seconds
-
+% Define MPC controllers for the 4 subsystem
 mpc_x = MpcControl_x(sys_x, Ts, H);
 mpc_y = MpcControl_y(sys_y, Ts, H);
 mpc_z = MpcControl_z(sys_z, Ts, H);
 mpc_roll = MpcControl_roll(sys_roll, Ts, H);
-
-
 
 % Merge four sub−system controllers into one full−system controller
 mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
@@ -33,18 +32,14 @@ ref4 = [2 2 2 deg2rad(40)]';
 U_opt(:,end+1) = nan;
 
 ph = rocket.plotvis(T_opt, X_opt, U_opt, ref4); % Plot as usual
-ph.fig.Name = 'booohhh';
+ph.fig.Name = 'Linear MPC in nonlinear system - Open Loop simulation'; 
 
 % Setup reference function
 ref = @(t_, x_) ref_TVC(t_);
-
-% Simulate
-H = 1;
-Tf = 35;
 
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
 
 % Visualize
 rocket.anim_rate = 10; % Increase this to make the animation faster
 ph = rocket.plotvis(T, X, U, Ref);
-ph.fig.Name = 'Merged lin. MPC in nonlinear simulation'; % Set a figure title
+ph.fig.Name = 'Linear MPC in nonlinear system - Closed Loop simulation'; 
