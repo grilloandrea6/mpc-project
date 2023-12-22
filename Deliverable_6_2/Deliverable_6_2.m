0b6b28f42036ed344786adf63d07bc9fe6c762c2
Andrea Grillo <andrea.grillo@epfl.ch>
2023-12-22 12:31:05 +0100

    note for 6.2

--- Deliverable_6_2/Deliverable_6_2.m
+++ Deliverable_6_2/Deliverable_6_2.m
@@ -1,5 +1,9 @@
 addpath(fullfile('..', 'src'));
 
+            for i = 1 : delay
+            %    x_ = RK4(x_,obj.mem_u,Ts,obj.rocket);
+        x_ = x_ + Ts*obj.rocket.f(x_,obj.mem_u);
+            end
 %close all
 %clear all
 %clc
