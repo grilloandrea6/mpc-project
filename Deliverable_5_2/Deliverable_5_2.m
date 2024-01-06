f73872ab79200278d2264afffd71b856d99e4d74
Andrea Grillo <andrea.grillo@epfl.ch>
2024-01-06 10:33:45 +0100

    copied files from 5.1 to 5.2

--- Deliverable_5_2/Deliverable_5_2.m
+++ Deliverable_5_2/Deliverable_5_2.m
@@ -1,8 +1,8 @@
 addpath(fullfile('..', 'src'));
 
-%close all
-%clear all
-%clc
+close all
+clear all
+clc
 
 %% TODO: This file should produce all the plots for the deliverable
 
@@ -15,21 +15,19 @@ sys = rocket.linearize(xs, us); % Linearize the nonlinear model about trim point
 
 
 % Design MPC controller
-H = 10; % Horizon length in seconds
+H = 1; % Horizon length in seconds
 
 mpc_x = MpcControl_x(sys_x, Ts, H);
 mpc_y = MpcControl_y(sys_y, Ts, H);
-mpc_z = MpcControl_z(sys_z, Ts, H); % to do , drop the terminal set, setup the estimator function, update the controller, and ..
+mpc_z = MpcControl_z(sys_z, Ts, H); 
 mpc_roll = MpcControl_roll(sys_roll, Ts, H);
 
 
 % Merge four sub−system controllers into one full−system controller
 mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
 
-
-% pad last input to get consistent size with time and state
-x0 = [zeros(1, 9), 1 0 3]' ;
-ref5 = [1.2, 0, 3, 0]';
+x0 = [zeros(1, 9), 1, 0, 3]' ;
+ref = [1.2, 0, 3, 0]';
 
 % Simulate
 Tf = 15;
@@ -38,7 +36,7 @@ Tf = 15;
 rocket.mass = 2.13;
 rocket.mass_rate = -0.27; %the mass is changing
 
-[T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref5, mpc_z, sys_z);
+[T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
 
 % Visualize
 rocket.anim_rate = 10; % Increase this to make the animation faster
