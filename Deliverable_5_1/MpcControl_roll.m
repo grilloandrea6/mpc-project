fd79339c555ebc191966cdbfa8f6dfed72df48c1
Andrea Grillo <andrea.grillo@epfl.ch>
2024-01-11 09:14:00 +0100

    updated mpc controllers in 5.1

--- Deliverable_5_1/MpcControl_roll.m
+++ Deliverable_5_1/MpcControl_roll.m
@@ -30,31 +30,14 @@ classdef MpcControl_roll < MpcControlBase
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
 
             % Horizon and cost matrices
-            Q = 10 * eye(2);
-            Q(2,2) = 70;
-            R = .01;
+            Q = 20 * eye(2);
+            R = 1;
 
             % u in U = { u | Mu <= m }
             M = [1;-1]; m = [20; 20];
    
             % Compute LQR controller for unconstrained system
-            [K,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
-            % MATLAB defines K as -K, so invert its sign
-            K = -K; 
-            
-            % Compute maximal invariant set
-            Xf = polytope(M*K,m);
-            Acl = mpc.A+mpc.B*K;
-            while 1
-                prevXf = Xf;
-                [T,t] = double(Xf);
-                preXf = polytope(T*Acl,t);
-                Xf = intersect(Xf, preXf);
-                if isequal(prevXf, Xf)
-                    break
-                end
-            end
-            [Ff,ff] = double(Xf);
+            [~,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
 
             % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
 
@@ -65,7 +48,6 @@ classdef MpcControl_roll < MpcControlBase
                 con = con + (M*U(:,i) <= m);
                 obj = obj + (X(:,i) - x_ref)' * Q * (X(:,i) - x_ref) + (U(:,i) - u_ref)' * R * (U(:,i) - u_ref);
             end
-            con = con + (Ff * (X(:,N) - x_ref) <= ff);
             obj = obj + (X(:,N) - x_ref)' * Qf * (X(:,N) - x_ref);
             
             
--- Deliverable_5_1/MpcControl_z.m
+++ Deliverable_5_1/MpcControl_z.m
@@ -44,32 +44,15 @@ classdef MpcControl_z < MpcControlBase
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
 
             % Horizon and cost matrices
-            Q = diag([0.005, 5000]);
-            R = .0003;
+            Q = diag([1 400]);
+            R = 0.1;
 
             % u in U = { u | Mu <= m }
             us = 56.6667;
             M = [1;-1]; m = [80-us; -(50-us)];
 
             % Compute LQR controller for unconstrained system
-            [K,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
-            % MATLAB defines K as -K, so invert its sign
-            K = -K; 
-
-            % Compute maximal invariant set
-            Xf = polytope(M * K, m);
-            Acl = mpc.A+mpc.B*K;
-            while 1
-                prevXf = Xf;
-                [T,t] = double(Xf);
-                preXf = polytope(T*Acl,t);
-                Xf = intersect(Xf, preXf);
-                if isequal(prevXf, Xf)
-                    break
-                end
-            end
-            [Ff,ff] = double(Xf);
-
+            [~,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
 
             % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj
             con = (X(:,2) == mpc.A*X(:,1) + mpc.B*U(:,1)+ mpc.B*d_est) + (M*U(:,1) <= m); % added d_est
@@ -79,7 +62,6 @@ classdef MpcControl_z < MpcControlBase
                 con = con + (M*U(:,i) <= m);
                 obj = obj + (X(:,i) - x_ref)' * Q * (X(:,i) - x_ref) + (U(:,i) - u_ref)' * R * (U(:,i) - u_ref);
             end
-            con = con + (Ff * (X(:,N) - x_ref) <= ff); % could drop terminal set
             obj = obj + (X(:,N) - x_ref)' * Qf * (X(:,N) - x_ref);
 
 
@@ -128,13 +110,7 @@ classdef MpcControl_z < MpcControlBase
 
             mat = [eye(size(mpc.A,1))-mpc.A -mpc.B; mpc.C zeros(size(mpc.C,1),size(mpc.B,2))];
 
-            con = ((mat * [xs; us]) == [mpc.B*d_est ; ref]); % modified need to include the disturbance in the steady state target
-
-
-            % u in U = { u | Mu <= m }
-            u_trim = 56.6667;
-            M = [1;-1]; m = [80-u_trim; -(50-u_trim)];
-            con = con + (M * us <= m);
+            con = ((mat * [xs; us]) == [mpc.B*d_est ; ref]);
 
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
